5eec3e1df29020c5597c6b3f20e3e0a117a53b1a
mod96 <58283520+mod96@users.noreply.github.com>
2022-10-17 00:27:23 +0900

    bash

---
+++ bash/kubeadm-init.bash
@@ -0,0 +1,15 @@
+sudo systemctl enable docker && systemctl start docker
+
+sudo systemctl enable kubelet && systemctl start kubelet
+
+cat <<EOF | sudo tee kubeadm-config.yaml
+kind: ClusterConfiguration
+apiVersion: kubeadm.k8s.io/v1beta3
+kubernetesVersion: v1.21.0
+---
+kind: KubeletConfiguration
+apiVersion: kubelet.config.k8s.io/v1beta1
+cgroupDriver: systemd
+EOF
+
+kubeadm init --config kubeadm-config.yaml
