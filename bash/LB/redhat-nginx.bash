56d0bbe06d2a088e01d4fc0eadb3da61baafbe4f
mod96 <58283520+mod96@users.noreply.github.com>
2022-10-17 22:17:53 +0900

    bash

--- bash/LB/redhat-nginx.bash
+++ bash/LB/redhat-nginx.bash
@@ -1,9 +1,21 @@
+sudo yum install yum-utils
+
 cat <<EOF | sudo tee /etc/yum.repos.d/nginx.repo
-[nginx]
-name=nginx repo
-baseurl=http://nginx.org/packages/centos/7/$basearch/
-gpgcheck=0
+[nginx-stable]
+name=nginx stable repo
+baseurl=http://nginx.org/packages/centos/$releasever/$basearch/
+gpgcheck=1
 enabled=1
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
+
+[nginx-mainline]
+name=nginx mainline repo
+baseurl=http://nginx.org/packages/mainline/centos/$releasever/$basearch/
+gpgcheck=1
+enabled=0
+gpgkey=https://nginx.org/keys/nginx_signing.key
+module_hotfixes=true
 EOF
 
 sudo yum install -y nginx
